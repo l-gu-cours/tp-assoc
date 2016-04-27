fce6b2ff5c203bf87aed27832e9bcdda96fa3559
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-04-27 17:47:29 +0200

    Check required parameters

--- src/main/java/org/tp/servlet/actions/EnregistrerCompteServlet.java
+++ src/main/java/org/tp/servlet/actions/EnregistrerCompteServlet.java
@@ -1,6 +1,8 @@
 package org.tp.servlet.actions ;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,9 +25,33 @@ public class EnregistrerCompteServlet extends GenericActionServlet
 	}
 	
     //-----------------------------------------------------------------------------
+    protected List<String> checkRequiredParameters(HttpServletRequest request, String...paramNames) throws ServletException, IOException
+    {
+    	List<String> missingParameters = new LinkedList<>();
+    	for ( String name : paramNames ) {
+    		String v = request.getParameter(name) ;
+        	trace(" parameter '" + name + "' = '" + v + "'" ); 
+    		if ( v == null ) {
+    			missingParameters.add(name);
+    		}
+    		else if ( v.trim().length() == 0 ) {
+    			missingParameters.add(name);
+    		}
+    	}
+    	return missingParameters ;
+    }
+
+    //-----------------------------------------------------------------------------
     protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-		forward(request, response, Page.WELCOME );
+    	List<String> missingParameters = checkRequiredParameters(request, "ident", "mp1","mp2", "nom", "prenom");
+    	trace("missing parameter(s) : " + missingParameters.size() ); 
+    	if ( missingParameters.size() > 0 ) {
+    		forward(request, response, Page.FORM_COMPTE );
+    	}
+    	else {
+    		forward(request, response, Page.WELCOME );
+    	}
     }
 
 }
